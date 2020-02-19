77c1af8b3292acec55da64b127bb5b5344d15206
Baptiste Trs <tbweb13@gmail.com>
2020-02-19 11:06:02 +0100

    first commit with checkbox design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projet-demo';
+  form: FormGroup;
+
+  days = [
+    {id: 0, name: 'Lundi'},
+    {id: 1, name: 'Mardi'},
+    {id: 2, name: 'Mercredi'},
+    {id: 3, name: 'Jeudi'},
+    {id: 4, name: 'Venderedi'},
+    {id: 5, name: 'Samedi'},
+    {id: 6, name: 'Dimanche'}
+  ];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      days: new FormArray([])
+    });
+
+    this.addCheckboxes();
+  }
+
+  private addCheckboxes() {
+    this.days.forEach((o, i) => {
+      const control = new FormControl();
+      (this.form.controls.days as FormArray).push(control);
+    });
+  }
+
+  getControls() {
+    return (this.form.controls.days as FormArray).controls;
+  }
+
+  get someChecked() {
+    return !this.form.value.days.some((v, i) => (v ? this.days[i].id : null));
+  }
+
+  submit() {
+    const selectedDaysIds = this.form.value.days
+      .map((v, i) => (v ? this.days[i].id : null))
+      .filter(v => v !== null);
+    console.log(selectedDaysIds);
+  }
+
 }
